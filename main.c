f29ce973ec05ada36d31d9013b95158af9505da2
MicoBledsoe <MicoMods@gmail.com>
2022-11-19 10:04:53 -0800

    UPDATED

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
-hh
+#include "monty.h"
+
+char **theMONTEY;
+
+/**
+ * main - Entry point for monty
+ * @argc: argument count
+ * @argv: array of arg pointers
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+
