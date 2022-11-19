2411cbe4cc4397183d1e9f85acd05c2598a85e47
Frandy4ever <frandy4ever@gmail.com>
2022-11-19 10:14:32 -0800

    update main.c file argument

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include "monty.h"
 
-char **theMONTEY;
-
 /**
- * main - Entry point for monty
- * @argc: argument count
- * @argv: array of arg pointers
- * Return: Always 0 (Success)
+ * main - main function for monty interpreter
+ * @argv: arguments for monty language
+ * @argc: numbers of arguments for monty command
+ * Return: the action of the command on monty language
  */
 int main(int argc, char *argv[])
 {
 
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	_getline(argv[1]);
+	return (0);
+}
